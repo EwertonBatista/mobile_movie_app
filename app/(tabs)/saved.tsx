46e778516a5c01c50af12559d54b9a5b8f863634
Ewerton Batista <ewerton.batista@prexis.com.br>
2025-11-08 21:57:46 -0300

    Refactor saved movies component by removing commented code and adjusting padding for better layout.

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -26,12 +26,9 @@ useFocusEffect(
 
   return (
     <View className='flex-1 bg-primary'>
-        {/* <View className='mb-20'> */}
           <View>
             <Image source={icons.logo} className='w-12 h-10 mt-20 mb-5 mx-auto z-1'/>
           </View>
-          {/* <Text className='text-white font-bold text-xl text-center'>Favoritados</Text> */}
-        {/* </View> */}
         <Image source={images.bg} className="absolute w-full z-0"/>
         
           <View className=''>
@@ -44,21 +41,12 @@ useFocusEffect(
               columnWrapperStyle={{
                   justifyContent: 'flex-start',
                   gap: 20,
-                  paddingRight: 5,
+                  paddingRight: 10,
+                  paddingLeft: 10,
                   marginBottom: 10
                 }}
               renderItem={({ item }) => (                
-
                 <MovieCard {...item} id={item.movie_id}/>
-
-                // <Link href={`/movies/${item.movie_id}`} asChild>
-                //   <TouchableOpacity className='flex-row items-center justify-center w-32 relative pl-5'>
-                //     <View className='flex flex-col'>
-                //       <Image source={{uri: item.poster_url}} className='w-32 h-48 rounded-lg' resizeMode='cover'/>
-                //       <Text className='text-white text-lg font-bold' numberOfLines={1}>{item.title}</Text>
-                //     </View>
-                //   </TouchableOpacity>
-                // </Link>
               )}
             />
           </View>
